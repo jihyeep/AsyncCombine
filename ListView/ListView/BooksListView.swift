cb9cc7b46d1374bb6b2eaba55f90dcdb0fd2ba98
Jihye Park <deviloper.park@gmail.com>
2024-06-17 17:08:23 +0900

    Searchable, refreshable 추가 with Combine

--- ListView/ListView/BooksListView.swift
+++ ListView/ListView/BooksListView.swift
@@ -6,14 +6,36 @@
 //
 
 import SwiftUI
+import Combine
+
+///
+private extension String {
+    func matches(_ searchTerm: String) -> Bool {
+        self.range(of: searchTerm, options: .caseInsensitive) != nil
+  }
+}
 
 private class BooksViewModel: ObservableObject {
-    @Published var books: [Book] = [Book]()
+    @Published var books: [Book] = Book.samples
     @Published var fetching = false
+    @Published var searchTerm: String = ""
+    
+    @Published var filteredBooks: [Book] = [Book]()
+    
+    init() {
+        Publishers.CombineLatest($books, $searchTerm).map { books, searchTerm in
+            books.filter { book in
+                searchTerm.isEmpty ? true : (book.title.matches(searchTerm) || book.author.matches(searchTerm))
+            }
+        }
+        .assign(to: &$filteredBooks)
+    }
 
     @MainActor
     func fetchData() async {
         fetching = true
+        /// List 위로 겹쳐서 progressBar가 그려지는 문제 해결
+        books.removeAll()
         do {
             /// MainActor와 await을 같이 쓰는 건 좋지 않음
             try await Task.sleep(for: .seconds(2))
@@ -28,8 +50,15 @@ struct BooksListView: View {
     @StateObject fileprivate var viewModel = BooksViewModel()
     
     var body: some View {
-        List(viewModel.books) { book in
+        List(viewModel.filteredBooks) { book in
             BookRowView(book: book)
+        }
+        /// 서치바
+        .searchable(text: $viewModel.searchTerm)
+        .textInputAutocapitalization(.none) ///
+        /// 당겨서 화면 새로고침
+        .refreshable {
+            await viewModel.fetchData()
         }
         .overlay {
             if viewModel.fetching {
@@ -67,5 +96,7 @@ private struct BookRowView: View {
 }
 
 #Preview {
-    BooksListView()
+    NavigationStack {
+        BooksListView()
+    }
 }
