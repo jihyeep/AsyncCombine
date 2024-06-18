675724df261d1570d26a6d7253daf73ed68e027e
Jihye Park <deviloper.park@gmail.com>
2024-06-18 13:24:08 +0900

    기본 Publisher 실행

--- GettingStaredWithCombine.playground/Pages/PizzaOrdering.xcplaygroundpage/Contents.swift
+++ GettingStaredWithCombine.playground/Pages/PizzaOrdering.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
 //: [Previous](@previous)
 
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+let pizzaOrder = Order()
+
+let pizzaOrderPublisher = NotificationCenter.default.publisher(for: .didUpdateOrderStatus, object: pizzaOrder)
+
+pizzaOrderPublisher.sink { Notification in
+    print(Notification)
+    dump(Notification)
+}
+
+pizzaOrderPublisher.map { Notification in
+    Notification.userInfo?["status"] as? OrderStatus ?? OrderStatus.placing
+}
+.sink { orderStatus in
+    print("Order status [\(orderStatus)")
+}
+
+// NotificationCenter를 사용하여 특정 이벤트가 발생했음을 다른 부분에 알리는 기능 수행
+NotificationCenter.default.post(name: .didUpdateOrderStatus, object: pizzaOrder, userInfo: ["status": OrderStatus.processing])
+
+print("Order: \(pizzaOrder.status)")
 
 //: [Next](@next)
