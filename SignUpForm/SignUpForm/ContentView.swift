0a0c137640c7ef105f324338ba98fcc97830a6a2
Jihye Park <deviloper.park@gmail.com>
2024-06-18 17:02:48 +0900

    Form validation with combine

--- SignUpForm/SignUpForm/ContentView.swift
+++ SignUpForm/SignUpForm/ContentView.swift
@@ -17,16 +17,49 @@ class SignUpFormViewModel: ObservableObject {
     @Published var passwordMessage: String = ""
     @Published var isValid: Bool = false
     
-    // Bool 타입, 에러는 Never
-    private lazy var isUsesrnameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+    private lazy var isUsesrnameLengthValidPublisher: AnyPublisher<Bool, Never> = { // Bool 타입, 에러는 Never
         $username.map { $0.count >= 3 }.eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordEmptyPublisher: AnyPublisher<Bool, Never> = {
+        $password.map(\.isEmpty).eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordMatchingPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest($password, $passwordConfirmation)
+            .map(==)
+//            .map { $0 == $1 } /// 위 코드와 동일
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { !$0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isFormValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isUsesrnameLengthValidPublisher, isPasswordValidPublisher)
+            .map { $0 && $1 }
+            .eraseToAnyPublisher()
     }()
     
     init() {
         // Combine을 사용하여 구독 형태로 변환
-        isUsesrnameLengthValidPublisher.assign(to: &$isValid)
+        isFormValidPublisher.assign(to: &$isValid)
         isUsesrnameLengthValidPublisher.map { $0 ? "" : "Username must be at least three characters!"}
             .assign(to: &$usernameMessage)
+        
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { isPasswordEmpty, isPasswordMatching in
+                if isPasswordEmpty {
+                    return "Password must not be empty"
+                } else if !isPasswordMatching {
+                    return "Passwords do not match"
+                }
+                return ""
+            }
+            .assign(to: &$passwordMessage)
     }
 }
 
