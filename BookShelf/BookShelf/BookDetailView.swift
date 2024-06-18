d6cbfb6ea1a0b42d7ef354df7d8173dc3dee58c3
Jihye Park <deviloper.park@gmail.com>
2024-06-18 09:18:23 +0900

    BookEditView 생성 및 편집

--- BookShelf/BookShelf/BookDetailView.swift
+++ BookShelf/BookShelf/BookDetailView.swift
@@ -20,14 +20,17 @@ struct BookDetailView: View {
                 .shadow(radius: 10)
                 .padding()
             Label(book.author, systemImage: "person.crop.rectangle")
-            Label("ISBN: \(book.isbn)", systemImage: "number")
-            Label("\(book.pages) pages", systemImage: "book")
-            Toggle("Read", isOn: .constant(book.isRead))
             Button(action: { showEditBookView.toggle() }) {
                 Label("Edit", systemImage: "pencil")
             }
+            Label("ISBN: \(book.isbn)", systemImage: "number")
+            Label("\(book.pages) pages", systemImage: "book")
+            Toggle("Read", isOn: .constant(book.isRead))
         }
         .navigationTitle(book.title)
+        .sheet(isPresented: $showEditBookView) {
+            BookEditView(book: $book)
+        }
     }
 }
 
