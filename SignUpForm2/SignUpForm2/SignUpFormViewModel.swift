d521b386441afde779bfff970764630de91d9893
Jihye Park <deviloper.park@gmail.com>
2024-06-20 15:09:58 +0900

    ViewModel error message 추가

--- SignUpForm2/SignUpForm2/SignUpFormViewModel.swift
+++ SignUpForm2/SignUpForm2/SignUpFormViewModel.swift
@@ -38,26 +38,51 @@ class SignUpFormViewModel: ObservableObject {
     }()
     
     init() {
+        // isValid
         isUsernameAvailablePublisher.map { result in
             switch result {
             case .success(let isAvailable):
                 return isAvailable
 //            case .failure(let error):
-            case .failure(_):
+            case .failure(let error):
+                if case APIError.transportError(_) = error {
+                    return true
+                }
                 return false
             }
         }
         .assign(to: &$isValid)
         
+        // usernameMessage
         isUsernameAvailablePublisher.map { result in
             switch result {
             case .success(let isAvailable):
                 return isAvailable ? "" : "This username is not available"
             // Error message
             case .failure(let error):
+                if case APIError.transportError(_) = error {
+                    return ""
+                }
+                else if case APIError.validationError(let reason) = error {
+                    return reason
+                }
+                else if case APIError.serverError(statusCode: _, reason: let reason, retryAfter: _) = error {
+                    return reason ?? "Server error"
+                }
                 return error.localizedDescription
             }
         }
         .assign(to: &$usernameMessage)
+        
+        // showUpdateDialog
+        isUsernameAvailablePublisher.map { result in
+            if case .failure(let error) = result {
+                if case APIError.decodingError = error {
+                    return true
+                }
+            }
+            return false
+        }
+        .assign(to: &$showUpdateDialog)
     }
 }
