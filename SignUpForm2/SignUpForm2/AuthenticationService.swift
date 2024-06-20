dfc22ac9d39856a40fd43dda3891bada12f18d05
Jihye Park <deviloper.park@gmail.com>
2024-06-20 11:46:35 +0900

    isUsernameAvailablePublisher 추가

--- SignUpForm2/SignUpForm2/AuthenticationService.swift
+++ SignUpForm2/SignUpForm2/AuthenticationService.swift
@@ -10,6 +10,7 @@ import Combine
 
 struct AuthenticationService {
     func checkUserNameAvailablePublisher(userName: String) -> AnyPublisher<Bool, Error> {
+        // 서버로 username을 보내 같은 데이터가 있는지 확인
         guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else {
             return Fail(error: APIError.invalidResponse)
                 .eraseToAnyPublisher()
--- SignUpForm2/SignUpForm2/SignUpFormViewModel.swift
+++ SignUpForm2/SignUpForm2/SignUpFormViewModel.swift
@@ -6,10 +6,30 @@
 //
 
 import Foundation
+import Combine
 
 class SignUpFormViewModel: ObservableObject {
+    typealias Available = Result<Bool, Error>
+    
     @Published var username: String = ""
     @Published var usernameMessage: String = ""
     @Published var isValid: Bool = false
     @Published var showUpdateDialog: Bool = false
+    
+    private var authenticationService = AuthenticationService()
+    
+    // Stream(publisher)
+    private lazy var isUsernameAvailablePublisher: AnyPublisher<Available, Never> = {
+        $username
+            .debounce(for: 0.5, scheduler: RunLoop.main) /// 속도 조절
+            .removeDuplicates() /// 중복 제거
+            // Stream 변경을 위해 사용
+            .flatMap { username -> AnyPublisher<Available, Never> in
+                self.authenticationService.checkUserNameAvailablePublisher(userName: username)
+                    // Stream에 들어가서 Result 타입으로 감싸서 값을 보냄
+                    .asResult()
+            }
+            .share()
+            .eraseToAnyPublisher()
+    }()
 }
