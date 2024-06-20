362aa6607050f6756dc51773af2d4e23894c6815
Jihye Park <deviloper.park@gmail.com>
2024-06-20 09:12:45 +0900

    Initialize SignUpForm2 project

---
+++ SignUpForm2/SignUpForm2/AuthenticationService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationService.swift
+//  SignUpForm2
+//
+//  Created by 박지혜 on 6/20/24.
+//
+
+import Foundation
---
+++ SignUpForm2/SignUpForm2/SignUpFormViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpFormViewModel.swift
+//  SignUpForm2
+//
+//  Created by 박지혜 on 6/20/24.
+//
+
+import Foundation
